c842af2a4edcd5183db4305815471902234d2e3b
rukundoeric <rukundoeric005@gmail.com>
2021-03-29 21:59:50 +0200

    implement header & change bookform according to the design

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import BooksList from './containers/BooksList';
 import BooksForm from './containers/BooksForm';
+import Header from './layouts/Header';
 import '../assets/css/style.css';
 
 function App() {
   return (
-    <div className="container">
-      <div className="container mt-5">
-        <BooksForm />
-      </div>
+    <div className="content">
+      <Header />
       <div className="container mt-3">
         <BooksList />
+      </div>
+      <div className="container mt-5">
+        <BooksForm />
       </div>
     </div>
   );
---
+++ src/components/layouts/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header className="header">
+      <div className="container d-flex justify-content-between">
+        <div className="d-flex left justify-content-center align-items-center">
+          <h6>BookStore CMS</h6>
+        </div>
+        <div className="d-flex right justify-content-center align-items-center">
+          <span className="d-flex justify-content-center align-items-center"><i>B</i></span>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
